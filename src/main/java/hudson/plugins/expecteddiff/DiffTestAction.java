89f74abc99025cb9dc604891f413df24e9e31fc6
Ryszard Perkowski <usultis@gmail.com>
2012-09-04 01:21:20 +0200

    moving logic to javascript, adding new supported assertions

--- src/main/java/hudson/plugins/expecteddiff/DiffTestAction.java
+++ src/main/java/hudson/plugins/expecteddiff/DiffTestAction.java
@@ -1,21 +1,13 @@
 package hudson.plugins.expecteddiff;
 
-import groovy.json.StringEscapeUtils;
 import hudson.tasks.junit.TestAction;
 import hudson.tasks.test.TestObject;
 
-import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import static groovy.json.StringEscapeUtils.escapeJavaScript;
 
 public class DiffTestAction extends TestAction {
 
-    private static final String OUTPUT_DIV_START = "<div id=\"comparison\"";
-    private static final String OUTPUT_DIV_END = "> </div>";
-
     private final TestObject testObject;
-    private final Differ diffier = new Differ();
     private final static Logger LOG = Logger.getLogger(DiffTestAction.class.getName());
 
     public DiffTestAction(TestObject testObject) {
@@ -32,24 +24,6 @@ public class DiffTestAction extends TestAction {
 
     public String getUrlName() {
         return null;
-    }
-
-    @Override
-    public String annotate(String text) {
-        LOG.log(Level.INFO,text);
-        Diff diff = diffier.diffy(text);
-        return diff != null ?
-                outputDiv(
-                        diff.getExpected(),
-                        diff.getActual()) + text
-                :
-                text;
-    }
-
-    private String outputDiv(String expected, String actual) {
-        LOG.log(Level.INFO,expected);
-        LOG.log(Level.INFO,actual);
-        return OUTPUT_DIV_START + " expected=\"" + expected + "\" actual=\""+ actual +"\"" + OUTPUT_DIV_END;
     }
 
     public TestObject getTestObject() {
--- src/main/webapp/expected.js
+++ src/main/webapp/expected.js
@@ -61,17 +61,40 @@ function prettyPrint(input) {
   }
 }
 
+function match (stacktrace) {
+var matched = stacktrace.match(/^[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+:[\s\S]+expected:&lt;([\s\S]*)&gt;[\s\S]+but[\s\S]+was:&lt;([\s\S]*)&gt;/);
+  if (matched != null)
+   return matched;
+  var sameMatched = stacktrace.match(/^[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+:[\s\S]+expected[\s\S]+same:&lt;([\s\S]*)&gt;[\s\S]+was[\s\S]+not:&lt;([\s\S]*)&gt;/);
+   if (sameMatched != null)
+     return sameMatched;
+  var isMatched =  stacktrace.match(/^[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+:[\s\S]+Expected:[\s\S]+is[\s\S]+&lt;([\s\S]*)&gt;[\s\S]+got:[\s\S]*&lt;([\s\S]*)&gt;/);
+  if (isMatched != null)
+   return isMatched;
+  var equalMatched = stacktrace.match(/^[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+:[\s\S]+Expected:[\s\S]+&lt;([\s\S]*)&gt;[\s\S]+got:[\s\S]*&lt;([\s\S]*)&gt;/);
+  if (equalMatched != null)
+     return equalMatched;
+  return
+    stacktrace.match(/^[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+:[\s\S]+expected[\s\S]+same[\s\S]+instance[\s\S]+but[\s\S]+found:&lt;([\s\S]*)&gt;[\s\S]+and:&lt;([\s\S]*)&gt;/);
+}
+
 function diffUsingJS () {
-	var diffoutputdiv = $("diffoutput");
-	var expected = dojo.attr("comparison","expected");
-    if(expected != null) {
-	var actual = dojo.attr("comparison","actual");
+    var stacktraceHeader = dojo.filter(dojo.query("h3"), function(header){ return header.innerHTML == 'Stacktrace'; })[0];
+ 
+    var stacktrace = stacktraceHeader.nextSibling.innerHTML;
+    if(stacktrace != undefined) {
+        var matched = match(stacktrace);
+    	if (matched != null) {
+    	var expected = matched[1];
+	    var actual = matched[2];
+    	var diffoutputdiv = $("diffoutput");
 	while (diffoutputdiv.firstChild) diffoutputdiv.removeChild(diffoutputdiv.firstChild);
 
         var exp = prettyPrint(expected);
         var act = prettyPrint(actual);
 	var output = prettydiff(apiParams(exp, act));
 	dojo.place(output[0],diffoutputdiv);
+	}
     }
 }
 dojo.addOnLoad(function() {
