45e5d9a6b5f2c7b1caef198a1e27ffb2e9d032d0
Ryszard Perkowski <usultis@gmail.com>
2012-09-07 00:42:56 +0200

    moving logic to javascript, adding new supported assertions

--- src/main/webapp/expected.js
+++ src/main/webapp/expected.js
@@ -50,32 +50,34 @@ function beautify(source) {
 
 function prettyPrint(input) {
 
-    if (input.match(/^([a-zA-Z0-9._\$]+(@[a-zA-Z0-9._\$]*){0,1}){0,1}\[.*]$/) != null)  {
+    if (input.match(/^([a-zA-Z0-9._\$]+(@[a-zA-Z0-9._\$]*){0,1}){0,1}\[.*(]|[.]{3})$/) != null)  {
+    if (input.endsWith("..."))
+      input = input+ "]";
     var start = input.indexOf('[');
     var prefix = input.substring(0, start);
     var ugly = input.substring(start).replace(/^\[/,"{").replace(/]$/,"}");
     var pretty = prettydiff(beautify(ugly))[0];
-    return prefix + pretty;
+    return prefix + pretty.replace(/^\{/,"[").replace(/}$/,"]");
   } else {
     return input;
   }
 }
 
 function match (stacktrace) {
-var matched = stacktrace.match(/^[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+:[\s\S]+expected:&lt;([\s\S]*)&gt;[\s\S]+but[\s\S]+was:&lt;([\s\S]*)&gt;/);
-  if (matched != null)
-   return matched;
-  var sameMatched = stacktrace.match(/^[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+:[\s\S]+expected[\s\S]+same:&lt;([\s\S]*)&gt;[\s\S]+was[\s\S]+not:&lt;([\s\S]*)&gt;/);
-   if (sameMatched != null)
-     return sameMatched;
-  var isMatched =  stacktrace.match(/^[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+:[\s\S]+Expected:[\s\S]+is[\s\S]+&lt;([\s\S]*)&gt;[\s\S]+got:[\s\S]*&lt;([\s\S]*)&gt;/);
-  if (isMatched != null)
-   return isMatched;
-  var equalMatched = stacktrace.match(/^[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+:[\s\S]+Expected:[\s\S]+&lt;([\s\S]*)&gt;[\s\S]+got:[\s\S]*&lt;([\s\S]*)&gt;/);
-  if (equalMatched != null)
-     return equalMatched;
-  return
-    stacktrace.match(/^[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+:[\s\S]+expected[\s\S]+same[\s\S]+instance[\s\S]+but[\s\S]+found:&lt;([\s\S]*)&gt;[\s\S]+and:&lt;([\s\S]*)&gt;/);
+    var patterns = [
+        /^[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+:[\s\S]+expected:&lt;([\s\S]*)&gt;[\s\S]+but[\s\S]+was:&lt;([\s\S]*)&gt;/,
+        /^[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+:[\s\S]+expected[\s\S]+same:&lt;([\s\S]*)&gt;[\s\S]+was[\s\S]+not:&lt;([\s\S]*)&gt;/,
+        /^[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+:[\s\S]+Expected:[\s\S]+is[\s\S]+&lt;([\s\S]*)&gt;[\s\S]+got:[\s\S]*&lt;([\s\S]*)&gt;/,
+        /^[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+:[\s\S]+Expected:[\s\S]+&lt;([\s\S]*)&gt;[\s\S]+got:[\s\S]*&lt;([\s\S]*)&gt;/,
+        /^[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+:[\s\S]+expected[\s\S]+same[\s\S]+instance[\s\S]+but[\s\S]+found:&lt;([\s\S]*)&gt;[\s\S]+and:[\s\S]*&lt;([\s\S]*)&gt;/
+        ];
+        for (i=0; i<patterns.length; i++)
+  {
+      var matched = stacktrace.match(patterns[i]);
+      if (matched != null)
+          return matched;
+  }
+  return null;
 }
 
 function diffUsingJS () {
@@ -86,7 +88,7 @@ function diffUsingJS () {
         var matched = match(stacktrace);
     	if (matched != null) {
     	var expected = matched[1];
-	    var actual = matched[2];
+	var actual = matched[2];
     	var diffoutputdiv = $("diffoutput");
 	while (diffoutputdiv.firstChild) diffoutputdiv.removeChild(diffoutputdiv.firstChild);
 
