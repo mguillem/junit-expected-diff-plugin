071328a5d6d7e99e85a2508de5e95ac03320776d
Marc Guillemot <mguillemot@yahoo.fr>
2020-08-26 16:03:58 +0200

    make working in pipeline with junit plugin 1.20

--- src/main/java/hudson/plugins/expecteddiff/DiffPublisher.java
+++ src/main/java/hudson/plugins/expecteddiff/DiffPublisher.java
@@ -1,22 +1,23 @@
 package hudson.plugins.expecteddiff;
 
-import hudson.Extension;
-import hudson.Launcher;
-import hudson.model.AbstractBuild;
-import hudson.model.BuildListener;
-import hudson.model.Descriptor;
-import hudson.tasks.junit.CaseResult;
-import hudson.tasks.junit.TestAction;
-import hudson.tasks.junit.TestDataPublisher;
-import hudson.tasks.junit.TestResult;
-import hudson.tasks.junit.TestObject;
-import hudson.tasks.junit.TestResultAction;
-
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
 import org.kohsuke.stapler.DataBoundConstructor;
+
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.Descriptor;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.tasks.junit.CaseResult;
+import hudson.tasks.junit.TestAction;
+import hudson.tasks.junit.TestDataPublisher;
+import hudson.tasks.junit.TestObject;
+import hudson.tasks.junit.TestResult;
+import hudson.tasks.junit.TestResultAction;
 
 public class DiffPublisher extends TestDataPublisher {
 
@@ -25,11 +26,9 @@ public class DiffPublisher extends TestDataPublisher {
     }
 
     @Override
-    public Data getTestData(AbstractBuild<?, ?> build, Launcher launcher,
-            BuildListener listener, TestResult testResult) throws IOException,
-            InterruptedException {
+    public TestResultAction.Data contributeTestData(Run<?, ?> run, FilePath workspace,
+    		Launcher launcher, TaskListener listener, TestResult testResult) throws IOException, InterruptedException {
         return new Data();
-
     }
 
     public static class Data extends TestResultAction.Data {
@@ -54,5 +53,4 @@ public class DiffPublisher extends TestDataPublisher {
         }
 
     }
-
 }
